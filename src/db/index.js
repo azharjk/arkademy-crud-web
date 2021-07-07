a96c1dd2772115702cf0d24baf6ebfab9c6d6f7b
azharjk <azhr.project@gmail.com>
2021-07-07 15:38:24 +0700

    added `getSingle`, `delete`, `update` functionality --- i think its done
     - added getSingleItem function
     - added deletItem function
     - added updateItem function
     - added modify pages
     - added new pages

--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,21 @@ INSERT INTO produk (nama_produk, keterangan, harga, jumlah)
 VALUES (?, ?, ?, ?);
 `;
 
+const sqlUpdateString = `
+UPDATE produk SET nama_produk = ?, keterangan = ?, harga = ?, jumlah = ?
+WHERE id = ?;
+`;
+
+const sqlDeleteString = `
+DELETE FROM produk WHERE id = ?;
+`;
+
 const sqlGetAllString = `
 SELECT * FROM produk;
+`;
+
+const sqlGetSingleString = `
+SELECT * FROM produk WHERE id = ?;
 `;
 
 const insertItem = ({ name, description, price, qty }) => {
@@ -16,6 +29,31 @@ const insertItem = ({ name, description, price, qty }) => {
         if (err) throw err;
       });
       db.endDBConnection(connection);
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+const updateItem = ({name, description, price, qty}, id) => {
+  db.createDBConnection()
+    .then(connection => {
+      connection.query(sqlUpdateString, [name, description, price, qty, id], (err, results, fields) => {
+        if (err) throw err;
+      });
+      db.endDBConnection(connection);
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+const deleteItem = (id) => {
+  db.createDBConnection()
+    .then(connection => {
+      connection.query(sqlDeleteString, [id], (err, results, fields) => {
+        if (err) throw err;
+      });
     })
     .catch(err => {
       throw err;
@@ -38,7 +76,27 @@ const getAllItem = () => {
   });
 }
 
+const getSingleItem = (id) => {
+  return new Promise((resolve, reject) => {
+    db.createDBConnection()
+      .then(connection => {
+        connection.query(sqlGetSingleString, [id], (err, results, fields) => {
+          if (err) reject(err);
+          resolve(results);
+        });
+      db.endDBConnection(connection);
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+
+}
+
 module.exports = {
   insertItem,
-  getAllItem
+  updateItem,
+  deleteItem,
+  getAllItem,
+  getSingleItem
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,47 @@ const db = require('./db');
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
   const items = await db.getAllItem();
   res.render('pages/index', { data: items });
+});
+
+app.get('/new', (req, res) => {
+  res.render('pages/new');
+});
+
+app.post('/new', (req, res) => {
+  db.insertItem({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    qty: req.body.qty
+  });
+  res.redirect('back');
+});
+
+app.get('/modify/:productId', async (req, res) => {
+  const item = await db.getSingleItem(req.params.productId);
+  res.render('pages/modify', { item: item[0] });
+});
+
+app.post('/modify/:productId', (req, res) => {
+  db.updateItem({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    qty: req.body.qty
+  }, req.params.productId);
+  res.redirect('/');
+});
+
+app.get('/delete/:productId', (req, res) => {
+  db.deleteItem(req.params.productId);
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
